8ecd5d9627c4610901b9dc3d9b0916862d4d9b27
kabilza <naruoluvohina@gmail.com>
2019-10-18 20:46:19 +0700

    fixed fraction_test.py

--- fraction.py
+++ fraction.py
@@ -57,8 +57,8 @@ class Fraction:
         if self.denominator == 1:
             str = self.numerator/self.denominator
             return f"{int(str)}"
-        elif self.denominator == 0 or self.numerator == 0:
-            return str(0)
+        # elif self.denominator == 0 or self.numerator == 0:
+        #     return str(0)
         else:
             return f"{self.numerator}/{self.denominator}"
 
